9f24ce46164fc94902e97bcfb2823d215ea9543b
TiaoVenanincio <tiaovenancio2@gmail.com>
2024-01-17 23:10:19 -0300

    :ok_hand: style func figuras de exemplo

--- src/TF_testador.py
+++ src/TF_testador.py
@@ -4,8 +4,42 @@ import matplotlib.pyplot as plt
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
 from sklearn.metrics import confusion_matrix, classification_report
 
+def exibe_9_exemplos(images, labels, predictions, class_names):
+    plt.figure(figsize=(12, 12))
+
+    # Define o espaçamento entre os subplots
+    plt.subplots_adjust(wspace=0.8, hspace=0.4)
+
+    for i in range(9):
+        plt.subplot(3, 3, i + 1)
+        plt.xticks([])
+        plt.yticks([])
+        plt.grid(False)
+
+        image = images[i]
+
+        if image.shape[-1] == 1:
+            image = np.squeeze(image)
+
+        plt.imshow(image)
+
+        predicted_label = np.argmax(predictions[i])
+
+        if predicted_label == np.argmax(labels[i]):
+            color = 'blue'
+            result_text = "Correto"
+        else:
+            color = 'red'
+            result_text = "Incorreto"
+
+        label_text = "True: " + class_names[np.argmax(labels[i])] + ", Pred: " + class_names[predicted_label] + f" ({result_text})"
+        plt.xlabel(label_text, color=color)
+
+    plt.show()
+
+
 # Carrega o modelo treinado e o lote de testes
-model_path = r"C:\Users\Sebastiao\Desktop\Projetos\projeto-ia-surface-defect\modelo_TF.h5"
+model_path = r"C:\Users\Sebastiao\Desktop\Projetos\projeto-ia-surface-defect\modelo_TF.keras"
 model1 = keras.models.load_model(model_path)
 
 test_dir = r"C:\Users\Sebastiao\Desktop\Projetos\projeto-ia-surface-defect\data\NEU Metal Surface Defects Data\test"
@@ -29,55 +63,25 @@ predictions = model1.predict(images)
 
 class_names=list(test_generator.class_indices.keys())
 
-plt.figure(figsize=(12, 12))
+exibe_9_exemplos(images, labels, predictions, class_names)
 
-# Defina o espaçamento entre os subplots
-plt.subplots_adjust(wspace=0.8, hspace=0.4)
-
-for i in range(9):
-    plt.subplot(3, 3, i + 1)
-    plt.xticks([])
-    plt.yticks([])
-    plt.grid(False)
-
-    image = images[i]
-
-    if image.shape[-1] == 1:
-        image = np.squeeze(image)
-
-    plt.imshow(image)
-
-    predicted_label = np.argmax(predictions[i])
-
-    if predicted_label == np.argmax(labels[i]):
-        color = 'blue'
-        result_text = "Correto"
-    else:
-        color = 'red'
-        result_text = "Incorreto"
-
-    label_text = "True: " + class_names[np.argmax(labels[i])] + ", Pred: " + class_names[predicted_label] + f" ({result_text})"
-    plt.xlabel(label_text, color=color)
-
-plt.show()
-
-# Obtenha todas as previsões para o conjunto de testes
+# Obtem todas as previsões para o conjunto de testes
 all_predictions = model1.predict(test_generator)
 
-# Converta as previsões em rótulos preditos
+# Converte as previsões em rótulos preditos
 predicted_labels = np.argmax(all_predictions, axis=1)
 
-# Converta as labels reais em rótulos reais
+# Converte as labels reais em rótulos reais
 true_labels = test_generator.classes
 
-# Crie a matriz de confusão
+# Cria a matriz de confusão
 confusion_mat = confusion_matrix(true_labels, predicted_labels)
 
-# Exiba a matriz de confusão
+# Exibe a matriz de confusão
 print("Matriz de Confusão:")
 print(confusion_mat)
 
-# Gere um relatório de classificação
+# Gera um relatório de classificação
 class_report = classification_report(true_labels, predicted_labels, target_names=class_names)
 print("\nRelatório de Classificação:")
 print(class_report)
