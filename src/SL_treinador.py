11423e2406284d4a161bfb5305b27c952b3b6141
TiaoVenanincio <tiaovenancio2@gmail.com>
2024-01-30 08:54:48 -0300

    :sparkles: feat cross validation

--- src/SL_treinador.py
+++ src/SL_treinador.py
@@ -1,10 +1,9 @@
 import joblib
 import os
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.model_selection import train_test_split
+from sklearn.model_selection import train_test_split, cross_val_score
 from sklearn.metrics import accuracy_score, confusion_matrix, classification_report
 import numpy as np
-
 
 def carrega_hist(imagem_path):
     #O objetivo dessa função é buscar os histogramas associados a uma determinada imagem.
@@ -22,7 +21,10 @@ def carrega_hist(imagem_path):
     except:
         print("O histograma não foi encontrado. Verifique os diretórios e se os histogramas foram criados corretamente.")
 
-
+def validacao_cruzada(modelo, X, y, n):
+    scores = cross_val_score(modelo, X, y, cv=n)
+    print("Acurácias em cada fold:", scores)
+    print("Acurácia média: {:.2f}\n\n".format(scores.mean()))
 
 # Define os caminhos dos diretórios
 data_dir = f"C:/Users/Sebastiao/Desktop/Projetos/projeto-ia-surface-defect/data"
@@ -55,7 +57,6 @@ y = np.array(rotulos)
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.1, random_state=42)
 
-#Aqui é testado um intervalo de valores para o classificador, tentando buscar um que trás a melhor acurácia
 modelo = RandomForestClassifier(n_estimators=175, random_state=42)
 
 modelo.fit(X_train, y_train)
@@ -65,17 +66,20 @@ previsoes = modelo.predict(X_test)
 precisao = accuracy_score(y_test, previsoes)
 print(f"N estimators: {175}, precisao do modelo: {precisao}")
 
-# Crie a matriz de confusão
 matriz_confusao = confusion_matrix(y_test, previsoes)
 
-# Exiba a matriz de confusão
 print("Matriz de Confusão:")
 print(matriz_confusao)
 
-# Gere um relatório de classificação
 class_report = classification_report(y_test, previsoes)
 print("\nRelatório de Classificação:")
 print(class_report)
 
 joblib.dump(modelo, 'modelo_SL.joblib')
 
+# Realizando a validação cruzada com n folds (8 neste caso)
+validacao_cruzada(modelo, X, y, 8)
+
+#O intuito de realizar a validacao cruzada é verificar se a acurácia do método padrao
+#desvia significativamente da acuracia da validacao cruzada, caso sim, pode-se apontar
+#um problema de overfitting.
